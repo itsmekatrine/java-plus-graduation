fc27cde7f129d573aacf16137e1ad49253902999
itsmekatrine <itsmekatrine1@gmail.com>
2025-10-30 13:53:05 +0300

    Настройки окружения для comment-service

---
+++ core/comment-service/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS public.comment (
+	id BIGSERIAL PRIMARY KEY,
+	event_id BIGINT NOT NULL,
+	author_id BIGINT,
+	content TEXT NOT NULL,
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS public.comment_pre_moderation (
+	event_id BIGINT NOT NULL,
+	forbidden_word varchar(32) NOT NULL,
+    PRIMARY KEY (event_id, forbidden_word)
+);
--- core/event-service/src/main/resources/schema.sql
+++ core/event-service/src/main/resources/schema.sql
@@ -46,27 +46,3 @@ CREATE TABLE IF NOT EXISTS public.event_compilation (
         REFERENCES public.compilation(id)
         ON DELETE CASCADE
 );
-
-
-CREATE TABLE IF NOT EXISTS public.comment (
-	id BIGSERIAL PRIMARY KEY,
-	event_id BIGINT NOT NULL,
-	author_id BIGINT,
-	content TEXT NOT NULL,
-	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT comment_event_fk FOREIGN KEY (event_id)
-		REFERENCES public."event"(id)
-		ON DELETE CASCADE,
-);
-
-CREATE TABLE IF NOT EXISTS public.comment_pre_moderation (
-	event_id BIGSERIAL NOT NULL,
-	forbidden_word varchar(32) NOT NULL,
-	CONSTRAINT comment_pre_moderation_pkey
-		PRIMARY KEY (event_id, forbidden_word),
-	CONSTRAINT comment_pre_moderation_event_fk
-		FOREIGN KEY (event_id)
-		REFERENCES public.event (id)
-		ON DELETE CASCADE
-);

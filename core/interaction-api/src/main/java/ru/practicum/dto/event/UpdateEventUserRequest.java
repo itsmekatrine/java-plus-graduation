6c6105637881b32d1105f9812a76a7da569d3ad4
itsmekatrine <itsmekatrine1@gmail.com>
2025-11-05 00:27:12 +0300

    Исправления для проверки

--- core/interaction-api/src/main/java/ru/practicum/dto/event/UpdateEventUserRequest.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/event/UpdateEventUserRequest.java
@@ -6,7 +6,6 @@ import lombok.experimental.FieldDefaults;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UpdateEventUserRequest extends UpdateEventRequest {
     UserEventAction stateAction;

400523e18ad265c08c781458f9e6f683237ce3a2
itsmekatrine <itsmekatrine1@gmail.com>
2025-11-11 15:42:03 +0300

    Правки для тестов

--- core/interaction-api/src/main/java/ru/practicum/dto/event/EventFullDto.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/event/EventFullDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.dto.category.CategoryDto;
@@ -17,17 +18,26 @@ public class EventFullDto {
     String annotation;
     CategoryDto category;
     Long confirmedRequests;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime eventDate;
+
     Long id;
     UserShortDto initiator;
     Boolean paid;
     String title;
     Double rating;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createdOn;
+
     String description;
     Location location;
     Integer participantLimit = 0;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime publishedOn;
+    
     Boolean requestModeration;
     EventState state;
     Set<String> forbiddenWords;
--- core/interaction-api/src/main/java/ru/practicum/dto/event/EventShortDto.java
+++ core/interaction-api/src/main/java/ru/practicum/dto/event/EventShortDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.dto.category.CategoryDto;
@@ -16,7 +17,10 @@ public class EventShortDto {
     String annotation;
     CategoryDto category;
     Long confirmedRequests;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime eventDate;
+
     Long id;
     UserShortDto initiator;
     Boolean paid;
